33d9f2196d431108ca86b42ed1a1d856ee3513fc
lxduonguetvnu <81974069+lxduonguetvnu@users.noreply.github.com>
2022-06-08 23:09:46 +0700

    Delete quansnet directory

--- quansnet/src/routes/drink.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const drinkController = require('../controllers/DrinkController');
-
-router.get('/:slug', drinkController.show_detail);
-router.get('/',drinkController.show_all);
-
-module.exports = router;
--- quansnet/src/routes/login.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const loginController = require('../controllers/loginController');
-
-router.use('/',loginController.index);
-
-module.exports = router;
